a0b561d91e98125a285bf0cab986b53c1a59edd6
Evil Kermit <cremonawow@gmail.com>
2025-12-12 19:57:51 +0200

    Assigned methods

--- Hackaton Skeleton (1)/Skeleton/src/com/company/core/strings/StringHelpers.java
+++ Hackaton Skeleton (1)/Skeleton/src/com/company/core/strings/StringHelpers.java
@@ -5,53 +5,53 @@ public class StringHelpers {
 
     public static String abbreviate(String source, int maxLength) {
         return null;
-    }
+    } // D
 
     public static String capitalize(String source) {
         return null;
-    }
+    } // D
 
     public static String concat(String string1, String string2) {
         return null;
-    }
+    } //D
 
     public static boolean contains(String source, char symbol) {
         return false;
-    }
+    } // A
 
     public static boolean endsWith(String source, char target) {
         return false;
-    }
+    } // A
 
     public static int firstIndexOf(String source, char target) {
         return 0;
-    }
+    } // A
 
     public static int lastIndexOf(String source, char symbol) {
         return 0;
-    }
+    } // S
 
-    public static String pad(String source, int length, char paddingSymbol) {
+    public static String pad(String source, int length, char paddingSymbol)  {
         return null;
 
-    }
+    } // S
 
     public static String padEnd(String source, int length, char paddingSymbol) {
         return null;
-    }
+    } // S
 
     public static String padStart(String source, int length, char paddingSymbol) {
         return null;
 
-    }
+    } // G
 
     public static String repeat(String source, int times) {
         return null;
-    }
+    } // G
 
     public static String reverse(String source) {
         return null;
-    }
+    } // G
 
     public static String section(String source, int start, int end) {
         return null;
